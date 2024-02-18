ed6eade2d58716164a05e0c2c296ca4d8915eb93
allhailpython <allhailpython@protonmail.com>
2024-02-18 00:12:56 +0000

    added django_filters to installed_apps

--- core/backend/settings/base.py
+++ core/backend/settings/base.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # added
+    "django_filters",
     "rest_framework",
     "reviews.apps.ReviewsConfig",
 ]
